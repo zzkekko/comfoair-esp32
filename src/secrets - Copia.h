00a878d0f9423800fe1db3179ec87b9775ae8581
Francesco <35064417+zzkekko@users.noreply.github.com>
2022-10-16 17:32:58 +0200

    Add files via upload

---
+++ src/secrets - Copia.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID "<your_ssid>"
+#define WIFI_PASS "<your_password>"
+#define MQTT_HOST "<ip_broker_mqtt>"
+#define MQTT_PORT 1883
+#define MQTT_PREFIX "<mqtt_topic_prefix>"
+#define MQTT_USER "<mqtt_user>"
+#define MQTT_PASS "<mqtt_passwrod"
